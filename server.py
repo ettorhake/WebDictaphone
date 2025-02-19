1f39fbeb935337518a04567e6e8399373fe50621
ettorhake <ettorhake@gmail.com>
2025-02-19 12:48:54 +0100

    Mise à jour du projet avec de nouvelles fonctionnalités

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, send_from_directory
 from database import get_recordings, delete_recording, init_db
 import os
+import logging
 
 app = Flask(__name__, static_folder='static')
+
+# Configurer le journalisation
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Initialiser la base de données
 init_db()
@@ -22,4 +26,13 @@ def remove_recording(recording_id):
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    cert_path = '...'
+    key_path = '...'
+    
+    if os.path.exists(cert_path) and os.path.exists(key_path):
+        context = (cert_path, key_path)
+        logging.debug(f'Certificat trouvé. Lancement du serveur en HTTPS sur le port 3443.')
+        app.run(debug=True, port=3443, ssl_context=context)  # Utiliser HTTPS
+    else:
+        logging.debug(f'Certificat non trouvé. Lancement du serveur en HTTP sur le port 5000.')
+        app.run(debug=True, port=5000)  # Utiliser HTTP
